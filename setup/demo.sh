59ff9c636df50920041ab27fa03006cb8d10a6d4
Karol Kurpanik <Karol.Kurpanik2@pl.ibm.com>
2020-02-25 17:39:23 +0100

    initialize setup directory

--- setup/demo.sh
+++ setup/demo.sh
@@ -7,14 +7,14 @@ if [ $1 = "up" ]; then
   kubectl -n k8s-cl1 port-forward --address 0.0.0.0 mongodb-7c758d7dbf-zll5z 50101:27017 &
   echo $! >> kubectl.pid
   echo "Starting mongo express..."
-  docker-compose -f mongo-express/docker-compose.yaml up -d
+  docker-compose -f ../lab/mongo-express/docker-compose.yaml up -d
   echo "Starting fauxton..."
-  docker-compose -f couchdb-fauxton/docker-compose.yaml up -d
+  docker-compose -f ../lab/couchdb-fauxton/docker-compose.yaml up -d
 elif [ $1 = "down" ]; then
   echo "Removing mongo express..."
-  docker-compose -f mongo-express/docker-compose.yaml down
+  docker-compose -f ../mongo-express/docker-compose.yaml down
   echo "Removing fauxton..."
-  docker-compose -f couchdb-fauxton/docker-compose.yaml down
+  docker-compose -f ../couchdb-fauxton/docker-compose.yaml down
   echo "Killing port forwarders ..."
   if [ -f "kubectl.pid" ]; then 
     for pid in `cat kubectl.pid`; do
