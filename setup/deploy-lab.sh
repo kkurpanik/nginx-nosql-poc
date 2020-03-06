724fb2a5173458e9b445e47694641d77bf1ded37
Karol Kurpanik <Karol.Kurpanik2@pl.ibm.com>
2020-03-06 17:10:17 +0100

    added mariadb to deploy-lab

--- setup/deploy-lab.sh
+++ setup/deploy-lab.sh
@@ -19,3 +19,10 @@ kubectl -n ${NAMESPACE} apply -f ../lab/mongodb/pvc.yaml
 kubectl -n ${NAMESPACE} apply -f ../lab/mongodb/secrets.yaml
 kubectl -n ${NAMESPACE} apply -f ../lab/mongodb/deployment.yaml
 kubectl -n ${NAMESPACE} apply -f ../lab/mongodb/service.yaml
+
+# deploy mariadb
+kubectl -n ${NAMESPACE} apply -f ../lab/mariadb/pv.yaml
+kubectl -n ${NAMESPACE} apply -f ../lab/mariadb/pvc.yaml
+kubectl -n ${NAMESPACE} apply -f ../lab/mariadb/secrets.yaml
+kubectl -n ${NAMESPACE} apply -f ../lab/mariadb/deployment.yaml
+kubectl -n ${NAMESPACE} apply -f ../lab/mariadb/service.yaml
