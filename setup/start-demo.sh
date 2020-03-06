f6fb13e75b7485754d50f8c1f22166638294a9dd
Karol Kurpanik <Karol.Kurpanik2@pl.ibm.com>
2020-03-06 18:17:02 +0100

    update start-demo

--- setup/start-demo.sh
+++ setup/start-demo.sh
@@ -7,21 +7,28 @@ if [ $1 = "up" ]; then
   # look for couchdb pod name
   COUCHDB_POD=`kubectl -n ${NAMESPACE} get pods | grep -i couchdb | awk '{print $1}'`
   MONGODB_POD=`kubectl -n ${NAMESPACE} get pods | grep -i mongodb | awk '{print $1}'`
+  MARIADB_POD=`kubectl -n ${NAMESPACE} get pods | grep -i mariadb | awk '{print $1}'`
 
   echo "Forwarding ports..."
   kubectl -n k8s-cl1 port-forward --address 0.0.0.0 ${COUCHDB_POD} 50201:5984 50203:5984 &
   echo $! >> kubectl.pid
   kubectl -n k8s-cl1 port-forward --address 0.0.0.0 ${MONGODB_POD} 50101:27017 50103:27017 &
   echo $! >> kubectl.pid
+  kubectl -n k8s-cl1 port-forward --address 0.0.0.0 ${MARIADB_POD} 50301:3306 &
+  echo $! >> kubectl.pid
   echo "Starting mongo express..."
   docker-compose -f ../lab/mongo-express/docker-compose.yaml up -d
   echo "Starting fauxton..."
   docker-compose -f ../lab/couchdb-fauxton/docker-compose.yaml up -d
+  echo "Starting phpmyadmin..."
+  docker-compose -f ../lab/phpmyadmin/docker-compose.yaml up -d
 elif [ $1 = "down" ]; then
   echo "Removing mongo express..."
   docker-compose -f ../lab/mongo-express/docker-compose.yaml down
   echo "Removing fauxton..."
   docker-compose -f ../lab/couchdb-fauxton/docker-compose.yaml down
+  echo "Removing phpmyadmin..."
+  docker-compose -f ../lab/phpmyadmin/docker-compose.yaml down
   
   echo "Killing port forwarders ..."
   if [ -f "kubectl.pid" ]; then 
